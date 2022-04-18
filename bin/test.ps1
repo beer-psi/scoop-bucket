e703f7a1eccc972732195bf749b83fa80f0da8d9
beerpiss <lacvtg.a1.2023@gmail.com>
2022-04-18 12:38:15 +0700

    ci: setup proper testing

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,2 +1,5 @@
+#Requires -Modules @{ ModuleName = 'Pester'; MaximumVersion = '4.99' }
+
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-Invoke-Pester "$psscriptroot/.."
+$result = Invoke-Pester "$psscriptroot/.." -PassThru
+exit $result.FailedCount
