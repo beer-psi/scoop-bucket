24682e97a1a50335d8a1de55d3f87981a929ea91
beerpiss <lacvtg.a1.2023@gmail.com>
2022-04-26 23:18:22 +0700

    feat,docs(msstore): add JSDoc, better error essages

--- endpoints/msstore.ts
+++ endpoints/msstore.ts
@@ -1,31 +1,94 @@
 import { DOMParser, NodeList } from "https://esm.sh/linkedom@0.14.7";
 import { HTMLDocument } from "https://cdn.esm.sh/v78/linkedom@0.14.7/types/html/document.d.ts";
 import WindowsLocale from "https://esm.sh/windows-locale@1.1.2";
+import validator from "https://esm.sh/validator@13.7.0";
 import {
   ReasonPhrases,
   StatusCodes,
 } from "https://esm.sh/http-status-codes@2.2.0";
 
+/**
+ * Upstream API from https://store.rg-adguard.net
+ */
 const UPSTREAM_API = "https://store.rg-adguard.net/api/GetFiles";
+
+/**
+ * Headers used for all responses throughout the API
+ */
 const CommonHeaders = {
   "content-type": "application/json; charset=UTF-8",
   "access-control-allow-origin": "*",
 };
 
+/**
+ * Regexes of valid Microsoft Store links
+ */
+const StoreRegexes = [
+  /^(https:\/\/)?apps\.microsoft\.com\/store\/detail\/.+\/[a-zA-Z0-9]+$/,
+  /^(https:\/\/)?microsoft\.com\/[a-zA-Z\-]+\/p\/.+\/[a-zA-Z0-9]+$/,
+  /^(https:\/\/)?microsoft\.com\/store\/productId\/[a-zA-Z0-9]+$/,
+];
+
+/**
+ * Describes a Microsoft Store download as parsed from UPSTREAM_API.
+ */
 interface StoreData {
+  /**
+   * ProductId of the package
+   */
   id: string;
+
+  /**
+   * Package version
+   */
   version: string;
+
+  /**
+   * Package architecture
+   */
   arch: string;
+
+  /**
+   * Information about the download
+   */
   file: {
+    /**
+     * Download link
+     */
     url: string;
+
+    /**
+     * Filename
+     */
     name: string;
+
+    /**
+     * File extension
+     */
     extension: string;
+
+    /**
+     * Download size
+     */
     size: string;
+
+    /**
+     * SHA1 checksum of the download
+     */
     sha1sum: string;
+    
+    /**
+     * The time when the download link expires
+     */
     expiry: string;
   };
 }
 
+/**
+ * Parses the HTML document given by UPSTREAM_API into StoreData[]
+ * @param document The parsed document returned from UPSTREAM_API
+ * @returns An array of downloads found from `document`.
+ */
 function parseDocument(document: HTMLDocument): StoreData[] {
   const rows: NodeList = document.querySelectorAll("tr");
   rows.shift();
@@ -50,9 +113,15 @@ function parseDocument(document: HTMLDocument): StoreData[] {
   });
 }
 
-function validateParams(sp: URLSearchParams): string {
+/**
+ * Validates user query parameters
+ * @param sp Some URL search parameters
+ * @returns An array of error messages, or an empty array if there are no errors
+ */
+function validateParams(sp: URLSearchParams): string[] {
+  const res: string[] = [];
   if (!sp.has("type") || !sp.has("url") || !sp.has("ring")) {
-    return "missing required parameters";
+    return ["missing required parameters"];
   }
   if (
     !["ProductId", "CategoryId", "url", "PackageFamilyName"].includes(
@@ -60,17 +129,46 @@ function validateParams(sp: URLSearchParams): string {
       sp.get("type"),
     )
   ) {
-    return "invalid type parameter";
+    res.push("invalid type parameter");
   }
   // @ts-ignore: Already validated
   if (!["Fast", "Slow", "RP", "Retail"].includes(sp.get("ring"))) {
-    return "invalid ring parameter";
+    res.push("invalid ring parameter");
   }
 
-  if (sp.has("lang") && !WindowsLocale[sp.get("lang")]) {
-    return "invalid language"
+  // @ts-ignore: sp.has("lang") confirms the existence
+  if (sp.has("lang") && !WindowsLocale[sp.get("lang")?.toLowerCase()]) {
+    res.push("invalid language");
   }
 
+  // @ts-ignore: already validated
+  if (validateUrl(sp.get("url"), sp.get("type"))) {
+    // @ts-ignore: shut the fuck up
+    res.push(validateUrl(sp.get("url"), sp.get("type")));
+  }
+
+  return res;
+}
+
+/**
+ * Validates the `url` query parameter by type
+ * @param url The `url` that needs validating
+ * @param type Type of the `url`
+ * @returns A string containing error, or a blank string if there is none.
+ */
+function validateUrl(
+  url: string,
+  type: "ProductId" | "CategoryId" | "url" | "PackageFamilyName",
+): string {
+  if (type === "CategoryId") {
+    return validator.isUUID(url) ? "" : "CategoryId not a UUID";
+  } else if (type === "url") {
+    return StoreRegexes.some((value: RegExp) => value.test(url))
+      ? ""
+      : "URL not valid. All URLs must be a Microsoft Store entry, with no query parameters";
+  } else if (type === "ProductId") {
+    return /^[a-zA-Z0-9]+$/.test(url) ? "" : "ProductId not valid";
+  }
   return "";
 }
 
@@ -78,14 +176,14 @@ export default async function handleRequest(
   request: Request,
 ): Promise<Response> {
   const sp = new URL(request.url).searchParams;
-  if (validateParams(sp)) {
+  if (validateParams(sp).length > 0) {
     return new Response(
       JSON.stringify(
         {
           message: "invalid parameters",
           reason: validateParams(sp),
           params: {
-            "type": "'ProductId' | 'CategoryId' | 'url' | 'PackageFamilyName",
+            "type": "'ProductId' | 'CategoryId' | 'url' | 'PackageFamilyName'",
             "url": "string",
             "ring": "'Fast' | 'Slow' | 'RP' | 'Retail'",
             "lang": "en-US",
