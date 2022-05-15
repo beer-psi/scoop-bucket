33f7acdb1116eac2b8cbc7e5c605db86fd6a00e0
beerpiss <lacvtg.a1.2023@gmail.com>
2022-05-15 08:46:39 +0700

    proper paths

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -187,7 +187,7 @@ hub diff --name-only | ForEach-Object {
 
     if ($Push) {
         Write-Host "Creating update $app ($version) ..." -ForegroundColor DarkCyan
-        python ../makeindex.py
+        python "$PSScriptRoot/../makeindex.py"
         execute "hub add $manifest"
         execute "hub add README.md"
 
